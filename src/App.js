2ef72d16e7a450f4603c9b59e86648e2924f4313
KevinThePepper <kevin.shelley@pm.me>
2019-08-19 13:17:15 -0400

    Used css module format to style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -65,18 +65,18 @@ class App extends Component {
             style.backgroundColor = 'red';
         }
 
-        const classes = [];
+        const assignedStyles = [];
         if (this.state.persons.length <= 2) {
-            classes.push('red')     // classes = ['red']
+            assignedStyles.push(classes.red)
         }
         if (this.state.persons.length <=1) {
-            classes.push('bold')    // classes = ['red', 'bold']
+            assignedStyles.push(classes.bold)
         }
 
         return (
-            <div className="App">
+            <div className={classes.App}>
                 <h1>Hi, I'm a react app</h1>
-                <p className={classes.join(' ')}>This is really working!</p>
+                <p className={assignedStyles.join(' ')}>This is really working!</p>
                 <button
                     style={style}
                     onClick={this.togglePersonsHandler}>Toggle Persons
