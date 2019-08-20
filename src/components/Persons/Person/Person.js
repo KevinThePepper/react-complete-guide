a166261be35ad44dda2e024b2075b6cab87bd00d
KevinThePepper <kevin.shelley@pm.me>
2019-08-20 13:57:20 -0400

    Demonstrating prop types to show how to define prop definitions

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,6 @@
 import React, {Component, Fragment} from 'react';
+import PropTypes from 'prop-types';
+
 import classes from './Person.css';
 import withClass from "../../../hoc/withClass";
 
@@ -18,5 +20,12 @@ class Person extends Component {
         );
     }
 }
+
+Person.propTypes = {
+    click: PropTypes.func,
+    name: PropTypes.string,
+    age: PropTypes.number,
+    changed: PropTypes.func,
+};
 
 export default withClass(Person, classes.Person);
