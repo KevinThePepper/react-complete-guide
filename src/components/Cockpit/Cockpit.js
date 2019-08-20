cc11474f507da45bd5950266760fdca46970b8be
KevinThePepper <kevin.shelley@pm.me>
2019-08-20 14:11:17 -0400

    Demonstrated using references on function-based component

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,13 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import classes from './Cockpit.css';
 
 const cockpit = props => {
+    const toggleBtnRef = useRef(null);
+
     useEffect(() => {
         console.log('[Cockpit.js] useEffect');
 
-        setTimeout(() => {
+/*        setTimeout(() => {
             alert('Saved data to cloud');
-        }, 1000);
+        }, 1000);*/
+        toggleBtnRef.current.click();
         return () => {
             console.log('[Cockpit.js] cleanup work in useEffect');
         }
@@ -38,6 +41,7 @@ const cockpit = props => {
             <h1>{props.title}</h1>
             <p className={assignedStyles.join(' ')}>This is really working!</p>
             <button
+                ref={toggleBtnRef}
                 className={btnClass}
                 onClick={props.clicked}>Toggle Persons
             </button>
